6afad8ef48c23e2b70b5234f13fe216e357c2340
Carles Antón <carles.anton97@gmail.com>
2020-04-26 22:26:28 +0200

    refacotr: deleted files

--- untitled.py
+++
@@ -1,7 +0,0 @@
-import folder_patches
-
-folder_path='/Volumes/Data_HD/Users/Carles/TFG/image data for tests'
-dest_path='/Volumes/Data_HD/Users/Carles/TFG/patches'
-
-
-folder_patches.patch_folder(5,1,folder_path,dest_path,'jpg')
