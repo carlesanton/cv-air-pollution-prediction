400a19a0f0d7be9b9977fa8e7d4d0eb129076aaf
Carles Antón <carles.anton97@gmail.com>
2020-04-26 22:39:02 +0200

    feat: scripts to manage the DB

---
+++ database/amos_database_creation.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__),'..','..'))
+from amos3 import amos3
+
+
+amos3.client.get_camera_list()
+
+https://github.com/GWUvision/AMOSEast
